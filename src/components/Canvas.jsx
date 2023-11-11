e332d28387adfaeaa102aca437a149ad05405400
NickZavadski <nick.mr.volk@gmail.com>
2023-11-12 01:30:25 +0300

    add brush and rect

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,11 +1,23 @@
+import { useEffect, useRef } from 'react'
+import { observer } from 'mobx-react-lite'
+import canvasState from '../store/canvasState'
+import toolState from '../store/toolState'
+import Brush from '../tools/Brush'
 import '../styles/canvas.scss'
 
-const Canvas = () => {
+const Canvas = observer(() => {
+	const canvasRef = useRef(null)
+
+	useEffect(() => {
+		canvasState.setCanvas(canvasRef.current)
+		toolState.setTool(new Brush(canvasRef.current))
+	}, [])
+
 	return (
 		<div className="canvas">
-			<canvas width={600} height={400} />
+			<canvas width={600} height={400} ref={canvasRef} />
 		</div>
 	)
-}
+})
 
 export default Canvas
