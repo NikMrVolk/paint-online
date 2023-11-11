9fe6abccfcbb3f908746665f6d6d53e2070e29b7
NickZavadski <nick.mr.volk@gmail.com>
2023-11-12 00:33:33 +0300

    add basic model app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Canvas from './components/Canvas'
+import SettingBar from './components/SettingBar'
+import Toolbar from './components/Toolbar'
+import './styles/app.scss'
 
 function App() {
-  return (
-    <div className="App">work
-    </div>
-  );
+	return (
+		<div className="app">
+			<Toolbar />
+			<SettingBar />
+			<Canvas />
+		</div>
+	)
 }
 
-export default App;
+export default App
